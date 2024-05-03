f0312a05229616754151f6e4a21b337d158c067c
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-05-02 23:50:24 -0700

    Bleh, spell filename correctly

--- source/meta/check_errordlls.py
+++ source/meta/check_errordlls.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-error_dlls_path = os.path.join(".","resources","app","meta","manifests","exluded_dlls.json")
+error_dlls_path = os.path.join(".","resources","app","meta","manifests","excluded_dlls.json")
 if os.path.isfile(error_dlls_path):
     with open(error_dlls_path, "r") as error_dlls_file:
         error_dlls_json = json.load(error_dlls_file)
