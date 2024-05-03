7baaae3cbc260cbca477d0f700be16197f3ba282
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-05-02 23:45:50 -0700

    Try something else

--- source/meta/check_errordlls.py
+++ source/meta/check_errordlls.py
@@ -5,6 +5,6 @@ error_dlls_path = os.path.join(".","resources","app","meta","manifests","exluded
 if os.path.isfile(error_dlls_path):
     with open(error_dlls_path, "r") as error_dlls_file:
         error_dlls_json = json.load(error_dlls_file)
-        if len(error_dlls_json) > 0:
+        if len(error_dlls_json) > 0 and error_dlls_json[0].strip() != "":
             print(error_dlls_json)
             exit(1)
