6c98e94471b30a6181b8b6e53716e5b1d2254e30
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-05-03 21:18:14 -0700

    Fix version number building again

---
+++ source/classes/appversion.py
@@ -0,0 +1,18 @@
+import os
+
+from Main import __version__
+DR_VERSION = __version__
+
+from OverworldShuffle import __version__
+OWR_VERSION = __version__
+
+def write_appversion():
+  APP_VERSION = f"[OWR-{OWR_VERSION}][DR-{DR_VERSION}]"
+  APP_VERSION_FILE = os.path.join(".","resources","app","meta","manifests","app_version.txt")
+  with open(APP_VERSION_FILE,"w") as f:
+    f.seek(0)
+    f.truncate()
+    f.write(APP_VERSION)
+
+if __name__ == "__main__":
+    write_appversion()
