ca479283290240cf41331c2daa7f601b3392492e
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-05-05 20:41:56 -0700

    Update CI

--- source/classes/appversion.py
+++ source/classes/appversion.py
@@ -4,7 +4,9 @@ from OverworldShuffle import __version__
 OWR_VERSION = __version__
 
 def write_appversion():
-  APP_VERSION = f"v{OWR_VERSION}"
+  APP_VERSION = OWR_VERSION
+  if "-" in APP_VERSION:
+    APP_VERSION = APP_VERSION[:APP_VERSION.find("-")]
   APP_VERSION_FILE = os.path.join(".","resources","app","meta","manifests","app_version.txt")
   with open(APP_VERSION_FILE,"w") as f:
     f.seek(0)
