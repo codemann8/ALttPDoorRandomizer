86bb73fd238228eaf52b58d4585485165b8a4bd5
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-05-07 21:09:41 -0700

    Upload Logs instead of printing them

--- test/MysteryTestSuite.py
+++ test/MysteryTestSuite.py
@@ -109,6 +109,10 @@ if __name__ == "__main__":
 
     cpu_threads = args.cpu_threads
 
+    LOGPATH = os.path.join(".","logs")
+    if not os.path.isdir(LOGPATH):
+        os.makedirs(LOGPATH)
+
     for dr in [['mystery', args.count if args.count else 1, 1]]:
 
         for tense in range(1, dr[2] + 1):
@@ -123,7 +127,17 @@ if __name__ == "__main__":
             print()
 
             if errors:
-                with open(f"{dr[0]}{(f'-{tense}' if dr[0] in ['basic', 'crossed'] else '')}-errors.txt", 'w') as stream:
+                errors_filename = f"{dr[0]}"
+                if dr[0] in ["basic","crossed"]:
+                    errors_filename += f"-{tense}"
+                errors_filename += "-errors.txt"
+                with open(
+                    os.path.join(
+                        LOGPATH,
+                        errors_filename
+                    ),
+                    'w'
+                ) as stream:
                     for error in errors:
                         stream.write(error[0] + "\n")
                         stream.write(error[1] + "\n")
@@ -131,7 +145,7 @@ if __name__ == "__main__":
                         error[2] = error[2].split("\n")
                         results["errors"].append(error)
 
-    with open("mystery-success.txt", "w") as stream:
+    with open(os.path.join(LOGPATH, "mystery-success.txt"), "w") as stream:
         stream.write(str.join("\n", successes))
         results["success"] = successes
 
@@ -141,7 +155,7 @@ if __name__ == "__main__":
 
     print(f"Errors:  {num_errors}/{num_total}")
     print(f"Success: {num_success}/{num_total}")
-    print(results)
+    # print(results)
 
     if (num_errors/num_total) > (num_success/num_total):
         # exit(1)
