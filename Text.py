24603bdfd110114a160c40673e70321f0d2776c0
theclearmouse <105736589+theclearmouse@users.noreply.github.com>
2025-11-03 11:01:19 -0500

    update tournament winners in hint tiles

--- Text.py
+++ Text.py
@@ -1782,6 +1782,8 @@ class TextTable(object):
         text['hylian_text_2'] = CompressedTextMapper.convert("%%^= %==%\n ^ =%^=\n==%= ^^%^")
         text['desert_entry_translated'] = CompressedTextMapper.convert("Kneel before this stone, and magic will move around you.")
         text['telepathic_tile_under_ganon'] = CompressedTextMapper.convert("Doors Async League winners\n{HARP}\n"
+                                                                           "  ~~~2025~~~\nSchulzer\n\n"
+                                                                           "  ~~~2024~~~\nhumbugh\n\n"
                                                                            "  ~~~2023~~~\nEriror\n\n"
                                                                            "  ~~~2022~~~\nAndy\n\n"
                                                                            "  ~~~2021~~~\nprdwong")
@@ -1795,7 +1797,8 @@ class TextTable(object):
         text['telepathic_tile_ice_stalfos_knights_room'] = CompressedTextMapper.convert("{NOBORDER}\nKnock 'em down and then bomb them dead.")
         text['telepathic_tile_tower_of_hera_entrance'] = CompressedTextMapper.convert("{NOBORDER}\nThis is a bad place, with a guy who will make you fall…\n\n\na lot.")
         text['houlihan_room'] = CompressedTextMapper.convert("Randomizer tournament winners\n{HARP}\n"
-                                                             "  ~~~2023~~~\nnGanonsGoneWild\n\n"
+                                                             "  ~~~2024~~~\nGammachuu\n\n"
+                                                             "  ~~~2023~~~\nGanonsGoneWild\n\n"
                                                              "  ~~~2022~~~\nObscure\n\n"
                                                              "  ~~~2021~~~\nDaaanty\n\n"
                                                              "  ~~~2019~~~\nJet082\n\n"
@@ -2029,6 +2032,7 @@ class TextTable(object):
         text['ganon_phase_3_alt'] = CompressedTextMapper.convert("Got wax in your ears? I cannot die!")
         # 190
         text['sign_east_death_mountain_bridge'] = CompressedTextMapper.convert("Glitched\ntournament\nwinners\n{HARP}\n"
+                                                                               "~~~HMG 2025~~~\nSkele\n\n"
                                                                                "~~~No Logic 2024~~~\ntam\n\n"
                                                                                "~~~HMG 2023~~~\ntam\n\n"
                                                                                "~~~No Logic 2022~~~\nChexhuman\n\n"
